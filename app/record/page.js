f0b7fe43d2504dbfe93c09b41424c8f4f9ded1eb
Ahnaf Tajwar <tajwara@bu.edu>
2024-08-18 16:28:46 -0400

    updated buttons

--- app/record/page.js
+++ app/record/page.js
@@ -1,8 +1,13 @@
 import { Microphone } from "@/app/components/Microphone";
+import Link from "next/link";
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-4">
+      <Link href="/">
+        <Button variant="secondary" size="lg" sx={{my: 4}}>Stop Session</Button>
+      </Link>
       <div className="text-center mb-6">
         <h1 className="text-3xl font-bold text-gray-800 mb-2">
           Voice Chat
